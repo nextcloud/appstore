15a14e462f74f35e2ba115130faba80ff5220ebb
Bernhard Posselt <dev@bernhard-posselt.com>
2016-06-07 23:29:58 +0200

    Add filtering based on version for releases

--- nextcloudappstore/core/api/v1/views.py
+++ nextcloudappstore/core/api/v1/views.py
@@ -1,43 +1,12 @@
 from nextcloudappstore.core.api.v1.serializers import AppSerializer
 from nextcloudappstore.core.models import App
 from nextcloudappstore.core.permissions import AllowedToEditApp
+from nextcloudappstore.core.versioning import app_has_included_release
 from rest_framework import authentication
 from rest_framework.generics import GenericAPIView, DestroyAPIView
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
-from semantic_version import Version
-import sys
 
-
-def pad_max_version(version):
-    while version.count('.') < 2:
-        version += '.%i' % sys.maxsize
-    return version
-
-
-def pad_version(version):
-    while version.count('.') < 2:
-        version += '.0'
-    return version
-
-
-def includes_release(release, version_string):
-    version = Version(pad_version(version_string))
-    includes_min = True
-    includes_max = True
-    if release.platform_min:
-        min_version = pad_version(release.platform_min)
-        includes_min = Version(min_version) <= version
-    if release.platform_max:
-        max_version = pad_max_version((release.platform_max))
-        includes_max = Version(max_version) >= version
-    return includes_max and includes_min
-
-
-def app_has_included_release(app, version_string):
-    releases = app.releases.all()
-    releases = filter(lambda r: includes_release(r, version_string), releases)
-    return len(list(releases)) > 0
 
 
 class Apps(DestroyAPIView):
---
+++ nextcloudappstore/core/versioning.py
@@ -0,0 +1,37 @@
+from semantic_version import Version
+import sys
+
+
+def pad_max_version(version):
+    """
+    :argument version maximum version which is padded with the biggest number
+    this is because you want the maximum version 9.1 to be valid for 9.1.1
+    """
+    while version.count('.') < 2:
+        version += '.%i' % sys.maxsize
+    return version
+
+
+def pad_version(version):
+    while version.count('.') < 2:
+        version += '.0'
+    return version
+
+
+def includes_release(release, version_string):
+    version = Version(pad_version(version_string))
+    includes_min = True
+    includes_max = True
+    if release.platform_min:
+        min_version = pad_version(release.platform_min)
+        includes_min = Version(min_version) <= version
+    if release.platform_max:
+        max_version = pad_max_version((release.platform_max))
+        includes_max = Version(max_version) >= version
+    return includes_max and includes_min
+
+
+def app_has_included_release(app, version_string):
+    releases = app.releases.all()
+    releases = filter(lambda r: includes_release(r, version_string), releases)
+    return len(list(releases)) > 0
