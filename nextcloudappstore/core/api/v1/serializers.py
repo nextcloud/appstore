a0a1cbfb17c31b4cb1c9fb2d4b2be54af1968940
Bernhard Posselt <dev@bernhard-posselt.com>
2016-06-08 00:32:37 +0200

    add nice serializing

--- nextcloudappstore/core/api/v1/serializers.py
+++ nextcloudappstore/core/api/v1/serializers.py
@@ -1,8 +1,33 @@
-from nextcloudappstore.core.models import App, AppRelease
+from nextcloudappstore.core.models import *
 from rest_framework import serializers
 
 
+class PhpExtensionDependencySerializer(serializers.ModelSerializer):
+    id = serializers.ReadOnlyField(source='php_extension.id')
+    class Meta:
+        model = PhpExtensionDependency
+        fields = ('id', 'version_min', 'version_max')
+
+
+class DatabaseDependencySerializer(serializers.ModelSerializer):
+    id = serializers.ReadOnlyField(source='database.id')
+    name = serializers.ReadOnlyField(source='database.name')
+
+    class Meta:
+        model = DatabaseDependency
+        fields = ('id', 'name', 'version_min', 'version_max')
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ('id', 'name')
+
+
 class AppReleaseSerializer(serializers.ModelSerializer):
+    databases = DatabaseDependencySerializer(many=True, read_only=True, source='databasedependency_set')
+    libs = PhpExtensionDependencySerializer(many=True, read_only=True, source='phpextensiondependency_set')
+
     class Meta:
         model = AppRelease
         fields = ('version', 'libs', 'databases', 'shell_commands', 'php_min',
@@ -11,6 +36,7 @@ class AppReleaseSerializer(serializers.ModelSerializer):
 
 
 class AppSerializer(serializers.ModelSerializer):
+    categories = CategorySerializer(many=True, read_only=True)
     releases = AppReleaseSerializer(many=True, read_only=True)
 
     class Meta:
