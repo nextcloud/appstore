8f7abc54d572f648ddb83aa0f4876864018e2648
Adi Sieker <adi@sieker.io>
2016-06-12 20:42:05 +0200

    intial draft of home page
    adds category list and app list.

--- nextcloudappstore/core/views.py
+++ nextcloudappstore/core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
 
+from nextcloudappstore.core.mixins import CategoryContextMixin, RecommendedAppsContextMixin
 
-class HomeView(TemplateView):
+class HomeView(CategoryContextMixin, RecommendedAppsContextMixin, TemplateView):
     template_name = "home.html"
