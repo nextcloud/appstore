85fdaf726d18ece816a1f297a5c15f1e5b6d0f25
Jani Bonnevier <janibonnevier@users.noreply.github.com>
2016-07-04 14:33:27 +0200

    Remove needless code

--- nextcloudappstore/core/views.py
+++ nextcloudappstore/core/views.py
@@ -21,15 +21,16 @@ class CategoryAppListView(ListView):
     model = App
     template_name = 'app/list.html'
 
-    def get_queryset(self, request):
+    def get_queryset(self):
         category_id = self.kwargs['id']
         queryset = super().get_queryset()
 
         if category_id:
             queryset = queryset.filter(categories__id=category_id)
 
-        if ('search' in request.GET) and request.GET['search'].strip():
-            search_words = request.GET['search'].strip().split()
+        if ('search' in self.request.GET) \
+                and self.request.GET['search'].strip():
+            search_words = self.request.GET['search'].strip().split()
             query = None
 
             for word in search_words:
@@ -54,23 +55,3 @@ class CategoryAppListView(ListView):
         if category_id:
             context['current_category'] = Category.objects.get(id=category_id)
         return context
-
-    def get(self, request, *args, **kwargs):
-        self.object_list = self.get_queryset(request)
-        allow_empty = self.get_allow_empty()
-
-        if not allow_empty:
-            # When pagination is enabled and object_list is a queryset,
-            # it's better to do a cheap query than to load the unpaginated
-            # queryset in memory.
-            if (self.get_paginate_by(self.object_list) is not None
-                    and hasattr(self.object_list, 'exists')):
-                is_empty = not self.object_list.exists()
-            else:
-                is_empty = len(self.object_list) == 0
-            if is_empty:
-                raise Http404(
-                    _("Empty list and '%(class_name)s.allow_empty' is False.")
-                    % {'class_name': self.__class__.__name__})
-        context = self.get_context_data()
-        return self.render_to_response(context)
