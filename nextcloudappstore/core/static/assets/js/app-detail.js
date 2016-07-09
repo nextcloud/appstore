e5e8b8e65144bfadd684528860f6615a055df9d9
Bernhard Posselt <dev@bernhard-posselt.com>
2016-07-09 20:13:44 +0200

    move code into function block

--- nextcloudappstore/core/static/assets/js/app-detail.js
+++ nextcloudappstore/core/static/assets/js/app-detail.js
@@ -1,79 +1,81 @@
+(function (global) {
+    'use strict';
 
-class ImageSlider {
+    class ImageSlider {
 
-    constructor(element, autoscroll_interval, nextBtnElement,
-            prevBtnElement) {
-        this.element = element;
-        this.strip =
-            new ImageStrip(this, this.element.querySelector('.img-strip'));
-        this.curSlide = 0;
+        constructor(element, autoscroll_interval, nextBtnElement,
+                    prevBtnElement) {
+            this.element = element;
+            this.strip =
+                new ImageStrip(this, this.element.querySelector('.img-strip'));
+            this.curSlide = 0;
 
-        this.setSlide(this.curSlide);
+            this.setSlide(this.curSlide);
 
-        let slider = this;
+            let slider = this;
 
-        this.autoScroll = setInterval(function(){slider.increment(1)},
-            autoscroll_interval);
+            this.autoScroll = setInterval(function () {
+                    slider.increment(1)
+                },
+                autoscroll_interval);
 
-        nextBtnElement.addEventListener('click', function() {
-            slider.increment(1);
-            clearInterval(slider.autoScroll);
-        });
-        prevBtnElement.addEventListener('click', function() {
-            slider.increment(-1);
-            clearInterval(slider.autoScroll);
-        });
-    }
-
-    setSlide(slide) {
-        let imgSpacing = 4;
-        let imgWidth = this.element.offsetWidth + imgSpacing;
-        this.strip.setPosX(imgWidth * slide);
-        this.curSlide = slide;
-    }
-
-    increment(steps) {
-        let imgCount = this.strip.images.length;
-        let next = this.curSlide + steps;
-        next = ((next%imgCount)+imgCount)%imgCount; // because a simple % does it wrong
-        this.setSlide(next);
-    }
-}
-
-
-class ImageStrip {
-
-    constructor(slider, element) {
-        this.slider = slider;
-        this.element = element;
-        this.images = findImages(this.element);
-
-        function findImages(element) {
-            let imgElements = Array.from(element.querySelectorAll('.img'));
-            return imgElements.map(function(img) {
-                return new Image(img);
+            nextBtnElement.addEventListener('click', function () {
+                slider.increment(1);
+                clearInterval(slider.autoScroll);
             });
+            prevBtnElement.addEventListener('click', function () {
+                slider.increment(-1);
+                clearInterval(slider.autoScroll);
+            });
+        }
+
+        setSlide(slide) {
+            let imgSpacing = 4;
+            let imgWidth = this.element.offsetWidth + imgSpacing;
+            this.strip.setPosX(imgWidth * slide);
+            this.curSlide = slide;
+        }
+
+        increment(steps) {
+            let imgCount = this.strip.images.length;
+            let next = this.curSlide + steps;
+            next = ((next % imgCount) + imgCount) % imgCount; // because a simple % does it wrong
+            this.setSlide(next);
         }
     }
 
-    setPosX(posX) {
-        this.element.style.right = posX+'px';
+
+    class ImageStrip {
+
+        constructor(slider, element) {
+            this.slider = slider;
+            this.element = element;
+            this.images = findImages(this.element);
+
+            function findImages(element) {
+                let imgElements = Array.from(element.querySelectorAll('.img'));
+                return imgElements.map(function (img) {
+                    return new Image(img);
+                });
+            }
+        }
+
+        setPosX(posX) {
+            this.element.style.right = posX + 'px';
+        }
     }
-}
 
 
-class Image {
+    class Image {
 
-    constructor(element) {
-        this.element = element;
-        this.element.style.backgroundImage =
-            'url('+this.element.getAttribute('data-url')+')';
+        constructor(element) {
+            this.element = element;
+            this.element.style.backgroundImage =
+                'url(' + this.element.getAttribute('data-url') + ')';
+        }
     }
-}
 
 
-(function (global) {
-    'use strict';
     const AUTOSCROLL_INTERVAL = 8000;  // ms
     let document = global.document;
     let md = global.markdownit();
@@ -82,7 +84,7 @@ class Image {
     let imgSliderElement = document.getElementById('img-slider');
 
     let imgSlider = new ImageSlider(imgSliderElement, AUTOSCROLL_INTERVAL,
-            nextButton, prevButton);
+        nextButton, prevButton);
 
     let markdown = Array.from(document.querySelectorAll('.markdown'));
     markdown.forEach(elem => elem.innerHTML = md.render(elem.innerHTML));
