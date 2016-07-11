3bd1135bba97cf616fc7c990c12cb8b3f93a25da
Jani Bonnevier <janibonnevier@users.noreply.github.com>
2016-07-11 17:29:05 +0200

    Allow trailing slash at the end of URLs
    Specifically app detail page and category app list.

--- nextcloudappstore/urls.py
+++ nextcloudappstore/urls.py
@@ -7,9 +7,9 @@ from nextcloudappstore.core.views import CategoryAppListView, AppDetailView, \
 urlpatterns = [
     url(r'^$', CategoryAppListView.as_view(), {'id': None}, name='home'),
     url(r'^', include('allauth.urls')),
-    url(r'^categories/(?P<id>[\w]*)$', CategoryAppListView.as_view(),
+    url(r'^categories/(?P<id>[\w]*)/?$', CategoryAppListView.as_view(),
         name='category-app-list'),
-    url(r'^app/(?P<id>[\w_]+)$', AppDetailView.as_view(), name='app-detail'),
+    url(r'^app/(?P<id>[\w_]+)/?$', AppDetailView.as_view(), name='app-detail'),
     url(r'^app/(?P<id>[\w_]+)/description/?$', app_description,
         name='app-description'),
     url(r'^api/', include('nextcloudappstore.core.api.urls',
