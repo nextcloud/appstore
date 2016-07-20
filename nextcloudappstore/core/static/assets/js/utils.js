9235ff3ddac8f0f370bc42f01dc2306f62a278a3
Adi Sieker <adi@sieker.io>
2016-07-20 11:24:34 +0200

    Beginnings of account settings pages

--- nextcloudappstore/core/static/assets/js/utils.js
+++ nextcloudappstore/core/static/assets/js/utils.js
@@ -8,27 +8,4 @@
         return doc.body.innerHTML;
     };
 
-    global.fetchAPIToken = function (csrfToken) {
-        let request = new Request('/api/v1/token', {
-            method: 'POST',
-            headers: new Headers({
-                'Content-Type': 'application/json',
-                'X-CSRFToken': csrfToken,
-            }),
-            credentials: 'include'
-        });
-        return fetch(request).then(global.convertResponse);
-    };
-
-    global.convertResponse = function (response) {
-        if (response.status >= 200 && response.status < 400) {
-            if (response.headers.get('Content-Type') === 'application/json') {
-                return response.json();
-            } else {
-                return response.text();
-            }
-        }
-        return response.json().then(Promise.reject.bind(Promise));
-    }
-
 }(this));
