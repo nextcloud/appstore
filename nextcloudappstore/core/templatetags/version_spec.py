48b68f85050bf26c8ff2d1ded0b3ed5ec3690af9
Adi Sieker <adi@sieker.io>
2016-07-20 10:06:12 +0200

    decide on output of version_spec if the row is to be displayed or not.

--- nextcloudappstore/core/templatetags/version_spec.py
+++ nextcloudappstore/core/templatetags/version_spec.py
@@ -1,5 +1,4 @@
 from django import template
-from django.utils.translation import ugettext as _
 
 register = template.Library()
 
@@ -7,6 +6,6 @@ register = template.Library()
 @register.filter()
 def version_spec(value):
     if value == '*':
-        return _('all versions')
+        return ''
     else:
         return value
