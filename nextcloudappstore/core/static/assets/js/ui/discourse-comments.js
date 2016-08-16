12331fcce2ea7a00e2898988e71ef66fc43b2d67
Bernhard Posselt <bep@foryouandyourcustomers.com>
2016-08-16 22:23:28 +0200

    reorder some things

--- nextcloudappstore/core/static/assets/js/ui/discourse-comments.js
+++ nextcloudappstore/core/static/assets/js/ui/discourse-comments.js
@@ -1,9 +1,8 @@
-DiscourseEmbed = {
+window.DiscourseEmbed = {
     discourseUrl: 'https://help.nextcloud.com/'
 };
 
 (function() {
     let discourseUrl = document.getElementById('discourse-link').href;
     DiscourseEmbed.topicId = discourseUrl.split('/').pop();
-    console.log(DiscourseEmbed);
 })();
