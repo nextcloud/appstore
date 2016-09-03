2b11b52e7cea8f329ed9e8dd943201861c5d05bb
Bernhard Posselt <dev@bernhard-posselt.com>
2016-09-03 16:43:26 +0200

    fix style

--- nextcloudappstore/core/views.py
+++ nextcloudappstore/core/views.py
@@ -46,7 +46,6 @@ class AppDetailView(DetailView):
             rating.save()
         return redirect('app-detail', id=id)
 
-
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         try:
