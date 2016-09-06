7ce007232eb4de3506de7acffe2346ed3d5d146a
Bernhard Posselt <dev@bernhard-posselt.com>
2016-09-06 19:33:03 +0200

    generalize database translation export and import

--- nextcloudappstore/core/management/commands/createdbtranslations.py
+++ nextcloudappstore/core/management/commands/createdbtranslations.py
@@ -17,16 +17,24 @@ class Command(BaseCommand):
     help = (
         'Goes through all translated database models (hardcoded) and creates '
         'translations in %s' % translation_file)
+    translated_fields = (
+        (Category, ('name', 'description')),
+    )
+    source_lang = 'en'
 
     def handle(self, *args, **options):
         target_file = join(settings.BASE_DIR, self.translation_file)
-        categories = Category.objects.language('en').all()
-        strings = flatmap(lambda c: (c.name, c.description), categories)
-        strings = map(escape_tpl_string, strings)
-        wrapped_strings = map(lambda s: '{%% trans "%s" %%}' % s, strings)
-        content = '\n'.join(wrapped_strings)
+        translations = flatmap(lambda x: self._create_translations(x[0], x[1]),
+                           self.translated_fields)
+        content = '\n'.join(translations)
         with open(target_file, 'w') as f:
             f.write(content)
 
         msg = 'Exported translations to %s' % target_file
         self.stdout.write(self.style.SUCCESS(msg))
+
+    def _create_translations(self, model, fields):
+        objs = model.objects.language(self.source_lang).all()
+        strings = flatmap(lambda o: [getattr(o, f) for f in fields], objs)
+        strings = map(escape_tpl_string, strings)
+        return list(map(lambda s: '{%% trans "%s" %%}' % s, strings))
--- nextcloudappstore/core/management/commands/importdbtranslations.py
+++ nextcloudappstore/core/management/commands/importdbtranslations.py
@@ -1,59 +1,97 @@
+from functools import reduce
+
 from django.conf import settings
 from django.core.management import BaseCommand
+from django.core.management import CommandError
 from django.utils.translation import activate, deactivate, ugettext
+from parler.models import TranslatableModel
 
 from nextcloudappstore.core.models import Category
 
 
 class Command(BaseCommand):
     help = 'Imports all database translations from their respective .po files'
+    translated_fields = (
+        (Category, ('name', 'description')),
+    )
+    source_lang = 'en'
 
     def handle(self, *args, **options):
-        translated_langs = [lang[0] for lang in settings.LANGUAGES if
-                            lang[0] != 'en']
-        for code in translated_langs:
+        langs = [lang[0] for lang in settings.LANGUAGES if
+                 lang[0] != self.source_lang]
+        for code in langs:
             activate(code)
-            self._import_category_translations(code)
+            for model, attrs in self.translated_fields:
+                if issubclass(model, TranslatableModel):
+                    self._import_translations(model, attrs, code)
+                else:
+                    msg = 'Model "%s" is not translatable' % model.__name__
+                    raise CommandError(msg)
             deactivate()
 
         msg = 'Imported db translations'
         self.stdout.write(self.style.SUCCESS(msg))
 
-    def _import_category_translations(self, code):
-        # The following code is intended to be very confusing to read
-        for category in Category.objects.all():
-            category.set_current_language('en')
-            name_en = category.name
-            description_en = category.description
-            name_trans = ugettext(category.name)
-            description_trans = ugettext(category.description)
-            category.set_current_language(code)
-
-            if self._has_translations(category, name_trans, description_trans):
-                self._update_trans(category, 'name', code, name_en, name_trans)
-                self._update_trans(category, 'description', code,
-                                   description_en, description_trans)
-                category.save()
-
-    def _has_translations(self, category, name, description):
+    def _import_translations(self, model, attrs, code):
         """
-        There are no new translations if:
-         * the translations are the same as in the database
-         * the translations are empty
+        Import translations for fields on a model for a language code
+        :param model: the translated model
+        :param attrs: the attributes to translate
+        :param code: the language code
+        :return:
         """
-        return not (
-            (name.strip() == '' and description.strip() == '') or
-            (category.name == name and category.description == description)
-        )
+        for obj in model.objects.all():
+            obj.set_current_language(self.source_lang)
+            attrs_src = list(map(lambda f: getattr(obj, f), attrs))
+            attrs_trans = list(map(lambda f: ugettext(f), attrs_src))
+            obj.set_current_language(code)
 
-    def _update_trans(self, obj, attr, code, en, trans):
+            if self._has_translations(obj, attrs, attrs_trans):
+                for attr, src, trans in zip(attrs, attrs_src, attrs_trans):
+                    self._update_trans(obj, attr, code, src, trans)
+                obj.save()
+
+    def _has_translations(self, obj, attrs, attrs_trans):
+        """
+        Checks if any of the attributes has a translation
+        :param obj: translatable model instance
+        :param attrs: model attributes to check
+        :param attrs_trans: translated fields for the current attribute values
+        :return: True if the model has translations for the current code
+        """
+        translations_present = [self._has_translation(*(obj, attr, trans))
+                                for attr, trans
+                                in zip(attrs, attrs_trans)]
+        return reduce(lambda a, b: a or b, translations_present, False)
+
+    def _has_translation(self, obj, attr, trans):
+        """
+        Checks if an attribute has a translation
+        :param obj: translatable model instance
+        :param attrs: model attributes to check
+        :param trans: translated field for the current attribute value
+        :return: True if the attribute has translations for the current code
+        """
+        return trans.strip() != '' and getattr(obj, attr) != trans
+
+    def _update_trans(self, obj, attr, code, src, trans):
+        """
+        Updates a translation for an attribute on the object
+        :param obj: object which has the translatable attribute
+        :param attr: attribute name
+        :param code: language code
+        :param src: attribute value in the source language
+        :param trans: attribute value in the translated language
+        :return:
+        """
         if getattr(obj, attr) != trans:
-            msg = ('Update Category.%s for %s: "%s" -> "%s"'
-                   % (code, attr, getattr(obj, attr), trans))
+            msg = ('Update %s.%s for %s: "%s" -> "%s"'
+                   % (obj.__class__.__name__, attr, code, getattr(obj, attr),
+                      trans))
             self._print(msg)
             setattr(obj, attr, trans)
         else:
-            setattr(obj, attr, en)
+            setattr(obj, attr, src)
 
     def _print(self, msg):
         self.stdout.write(self.style.SUCCESS(msg))
