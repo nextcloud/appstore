271aeeade8a971a7f0f0f793e4b7f2d665cc3736
Bernhard Posselt <dev@bernhard-posselt.com>
2016-09-11 20:36:32 +0200

    Fix #253

---
+++ nextcloudappstore/core/api/v1/tests/test_app_register.py
@@ -0,0 +1,65 @@
+from django.contrib.auth import get_user_model
+from django.core.urlresolvers import reverse
+
+from nextcloudappstore.core.api.v1.tests.api import ApiTest
+from nextcloudappstore.core.facades import read_relative_file
+from nextcloudappstore.core.models import App, AppRelease
+
+
+class AppRegisterTest(ApiTest):
+    create_url = reverse('api:v1:app-register')
+    _cert = read_relative_file(__file__,
+                               'data/certificates/news-old.crt').strip()
+
+    def _create_app(self, owner, app_id):
+        return App.objects.create(owner=owner, id=app_id)
+
+    def test_register_unauthenticated(self):
+        response = self.api_client.post(self.create_url, data={
+            'signature': 'sign',
+            'certificate': 'cert'
+        }, format='json')
+        self.assertEqual(401, response.status_code)
+
+    def test_register_unauthorized(self):
+        owner = get_user_model().objects.create_user(username='owner',
+                                                     password='owner',
+                                                     email='owner@owner.com')
+        self._create_app(owner, 'news')
+        self._login_token()
+        response = self.api_client.post(self.create_url, data={
+            'signature': 'sign',
+            'certificate': self._cert
+        }, format='json')
+        self.assertEqual(403, response.status_code)
+
+    def test_register(self):
+        self._login()
+        with self.settings(VALIDATE_CERTIFICATES=False):
+            response = self.api_client.post(self.create_url, data={
+                'signature': 'sign',
+                'certificate': self._cert
+            }, format='json')
+            self.assertEqual(201, response.status_code)
+            app = App.objects.get(id='news')
+            self.assertEqual(self.user, app.owner)
+            self.assertEqual(self._cert, app.certificate)
+
+    def test_register_update(self):
+        app = self._create_app(self.user, 'news')
+        app.certificate = 'blubb'
+        app.save()
+        AppRelease.objects.create(version='1.0.0', app=app)
+
+        self.assertEqual(1, App.objects.get(id='news').releases.all().count())
+        self._login()
+        with self.settings(VALIDATE_CERTIFICATES=False):
+            response = self.api_client.post(self.create_url, data={
+                'signature': 'sign',
+                'certificate': self._cert
+            }, format='json')
+            self.assertEqual(200, response.status_code)
+            app = App.objects.get(id='news')
+            self.assertEqual(self.user, app.owner)
+            self.assertEqual(self._cert, app.certificate)
+            self.assertEqual(0, app.releases.all().count())
--- nextcloudappstore/core/certificate/validator.py
+++ nextcloudappstore/core/certificate/validator.py
@@ -12,7 +12,6 @@ logger = logging.getLogger(__name__)
 
 class CertificateConfiguration:
     def __init__(self) -> None:
-        self.validate_certs = settings.VALIDATE_CERTIFICATES
         self.digest = settings.CERTIFICATE_DIGEST
 
 
@@ -41,67 +40,53 @@ class CertificateValidator:
                            data: bytes) -> None:
         """
         Tests if a value is a valid certificate using SHA512
-        Logs an error if self.config.validate_certs is False
         :param certificate: the certificate to use as string
         :param signature: the signature base64 encoded string to test
         :param data: the binary file content that was signed
         :raises: InvalidSignatureException if the signature is invalid
         :return: None
         """
+        cert = self._to_cert(certificate)
+        err_msg = 'Signature is invalid'
         try:
-            cert = self._to_cert(certificate)
-            err_msg = 'Signature is invalid'
-            try:
-                result = verify(cert, b64decode(signature.encode()), data,
-                                self.config.digest)
-                if result is not None:
-                    raise InvalidSignatureException(err_msg)
-            except Exception as e:
-                raise InvalidSignatureException('%s: %s' % (err_msg, str(e)))
+            result = verify(cert, b64decode(signature.encode()), data,
+                            self.config.digest)
+            if result is not None:
+                raise InvalidSignatureException(err_msg)
         except Exception as e:
-            if self.config.validate_certs:
-                raise e
-            else:
-                logger.error(str(e))
+            raise InvalidSignatureException('%s: %s' % (err_msg, str(e)))
 
     def validate_certificate(self, certificate: str, chain: str,
                              crl: str = None) -> None:
         """
         Tests if a certificate has been signed by the chain, is not revoked
-        and has not yet been expired. Logs an error if
-        self.config.validate_certs is False
+        and has not yet been expired.
         :param certificate: the certificate to test as string
         :param chain: the certificate chain file content as string
         :param crl: the certificate revocation list file content as string
         :raises: InvalidCertificateException if the certificate is invalid
         :return: None
         """
+        # root and intermediary certificate need to be split
+        cas = pem.parse(chain.encode())
+        store = X509Store()
+        for ca in cas:
+            store.add_cert(self._to_cert(str(ca)))
+
+        cert = self._to_cert(certificate)
+        ctx = X509StoreContext(store, cert)
+        err_msg = 'Certificate is invalid'
+
+        if crl:
+            crl = load_crl(FILETYPE_PEM, crl)
+            store.add_crl(crl)
+
         try:
-            # root and intermediary certificate need to be split
-            cas = pem.parse(chain.encode())
-            store = X509Store()
-            for ca in cas:
-                store.add_cert(self._to_cert(str(ca)))
-
-            cert = self._to_cert(certificate)
-            ctx = X509StoreContext(store, cert)
-            err_msg = 'Certificate is invalid'
-
-            if crl:
-                crl = load_crl(FILETYPE_PEM, crl)
-                store.add_crl(crl)
-
-            try:
-                result = ctx.verify_certificate()
-                if result is not None:
-                    raise InvalidCertificateException(err_msg)
-            except Exception as e:
-                raise InvalidCertificateException('%s: %s' % (err_msg, str(e)))
+            result = ctx.verify_certificate()
+            if result is not None:
+                raise InvalidCertificateException(err_msg)
         except Exception as e:
-            if self.config.validate_certs:
-                raise e
-            else:
-                logger.error(str(e))
+            raise InvalidCertificateException('%s: %s' % (err_msg, str(e)))
 
     def get_cn(self, certificate: str) -> str:
         """
@@ -122,16 +107,10 @@ class CertificateValidator:
         not match
         :return: None
         """
-        try:
-            cn = self.get_cn(certificate)
-            if cn != app_id and self.config.validate_certs:
-                msg = 'App id %s does not match cert CN %s' % (app_id, cn)
-                raise CertificateAppIdMismatchException(msg)
-        except Exception as e:
-            if self.config.validate_certs:
-                raise e
-            else:
-                logger.error(str(e))
+        cn = self.get_cn(certificate)
+        if cn != app_id:
+            msg = 'App id %s does not match cert CN %s' % (app_id, cn)
+            raise CertificateAppIdMismatchException(msg)
 
     def _to_cert(self, certificate: str) -> X509:
         return load_certificate(FILETYPE_PEM, certificate.encode())
