0d6587914d9f6273ff8b0e8aadf033524386ed2b
Bernhard Posselt <dev@bernhard-posselt.com>
2016-09-13 20:30:35 +0200

    fix crl

--- nextcloudappstore/core/certificate/tests/test_validator.py
+++ nextcloudappstore/core/certificate/tests/test_validator.py
@@ -40,8 +40,7 @@ class ValidatorTest(TestCase):
 
     def test_validate_cert_not_signed(self) -> None:
         cert = self._read_cert('app.crt')
-        chain = self._read_cert(
-            'nextcloud.crt')
+        chain = self._read_cert('imaginary.chain')
         with(self.assertRaises(InvalidCertificateException)):
             self.validator.validate_certificate(cert, chain)
 
