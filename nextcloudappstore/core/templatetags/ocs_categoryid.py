a3437046e8824dc30f5240b11914705beec45f95
Bernhard Posselt <dev@bernhard-posselt.com>
2016-09-30 17:15:43 +0200

    revert ocs fixes for ints

--- nextcloudappstore/core/templatetags/ocs_categoryid.py
+++
@@ -1,8 +0,0 @@
-from django import template
-
-register = template.Library()
-
-
-@register.filter(name='ocs_categoryid')
-def ocs_categoryid(id: str) -> str:
-    return ''.join(map(lambda char: str(ord(char)), id[:4]))
