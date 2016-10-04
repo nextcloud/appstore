f55e91123fb301516b6f30945e625ea48b6345e7
Bernhard Posselt <dev@bernhard-posselt.com>
2016-10-04 21:27:14 +0200

    merge migrations

--- nextcloudappstore/core/migrations/0006_apprelease_changelog.py
+++ nextcloudappstore/core/migrations/0006_apprelease_changelog.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('core', '0004_appownershiptransfer'),
+        ('core', '0005_app_ocsid'),
     ]
 
     operations = [
