9a1fd795123147f4367f6d3b56b5a023b2dff0b8
Jani Bonnevier <jani@bonnevier.one>
2017-03-17 18:32:36 +0100

    Shorter test string

--- nextcloudappstore/core/static/assets/app/dom/TemplatingSpec.ts
+++ nextcloudappstore/core/static/assets/app/dom/TemplatingSpec.ts
@@ -3,8 +3,9 @@ import {noReferrerLinks} from './Templating';
 describe('HTML templating utilities', () => {
 
     it('should add rel="noreferrer noopener"', () => {
-        const html = 'Lorem <a href="#">ipsum dolor</a> sit amet, <a href="http://example.com">consectetur <strong>adipiscing</strong></a> elit. Praesent feugiat mauris <a href="http://example.com" target="_blank">non dapibus mattis</a>. Nunc luctus, lacus vitae.';
-        const expected = 'Lorem <a href="#" rel="noopener noreferrer">ipsum dolor</a> sit amet, <a href="http://example.com" rel="noopener noreferrer">consectetur <strong>adipiscing</strong></a> elit. Praesent feugiat mauris <a href="http://example.com" target="_blank" rel="noopener noreferrer">non dapibus mattis</a>. Nunc luctus, lacus vitae.';
+        const html = 'Lorem <a href="#">ipsum </a> sit';
+        const expected =
+            'Lorem <a href="#" rel="noopener noreferrer">ipsum </a> sit';
         expect(noReferrerLinks(html)).toEqual(expected);
     });
 
