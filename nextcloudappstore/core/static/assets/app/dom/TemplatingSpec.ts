1884169a3df66e098825eed562f519ab4cc8fc97
Jani Bonnevier <jani@bonnevier.one>
2017-03-17 18:22:54 +0100

    Add test for noReferrerLinks

--- nextcloudappstore/core/static/assets/app/dom/TemplatingSpec.ts
+++ nextcloudappstore/core/static/assets/app/dom/TemplatingSpec.ts
@@ -3,7 +3,9 @@ import {noReferrerLinks} from './Templating';
 describe('HTML templating utilities', () => {
 
     it('should add rel="noreferrer noopener"', () => {
-        fail();
+        const html = 'Lorem <a href="#">ipsum dolor</a> sit amet, <a href="http://example.com">consectetur <strong>adipiscing</strong></a> elit. Praesent feugiat mauris <a href="http://example.com" target="_blank">non dapibus mattis</a>. Nunc luctus, lacus vitae.';
+        const expected = 'Lorem <a href="#" rel="noopener noreferrer">ipsum dolor</a> sit amet, <a href="http://example.com" rel="noopener noreferrer">consectetur <strong>adipiscing</strong></a> elit. Praesent feugiat mauris <a href="http://example.com" target="_blank" rel="noopener noreferrer">non dapibus mattis</a>. Nunc luctus, lacus vitae.';
+        expect(noReferrerLinks(html)).toEqual(expected);
     });
 
 });
