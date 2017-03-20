1e83f7ed8cd5398c425d065c0ea745213aedd92a
Bernhard Posselt <dev@bernhard-posselt.com>
2017-03-20 19:20:05 +0100

    remove more code

--- nextcloudappstore/core/static/assets/app/components/Accordion.ts
+++ nextcloudappstore/core/static/assets/app/components/Accordion.ts
@@ -1,15 +1,12 @@
 export class Accordion {
     private title: HTMLElement;
-    private content: HTMLElement;
 
     constructor(private elem: HTMLElement) {
         const title = elem.querySelector('.accordion-title') as HTMLElement;
-        const content = elem.querySelector('.accordion-content') as HTMLElement;
-        if (title === null || content === null) {
+        if (title === null) {
             throw new Error(`No content or title found for elem ${elem}`);
         }
         this.title = title;
-        this.content = content;
     }
 
     public attachEventListeners() {
