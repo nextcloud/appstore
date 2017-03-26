7aec1c6b278c7b2a9a0373e6d025358a0eb87be4
Bernhard Posselt <dev@bernhard-posselt.com>
2017-03-26 11:28:21 +0200

    make urls consistent

--- nextcloudappstore/core/user/urls.py
+++ nextcloudappstore/core/user/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     url(r'^$', AccountView.as_view(), name='account'),
     url(r'^transfer-apps/?$', TransferAppsView.as_view(),
         name='account-transfer-apps'),
-    url(r'^transfer-app/(?P<pk>[a-z0-9_]+)/?$', TransferAppsView.as_view(),
+    url(r'^transfer-apps/(?P<pk>[a-z0-9_]+)/?$', TransferAppsView.as_view(),
         name='account-transfer-app'),
     url(r'^password/?$', PasswordView.as_view(), name='account-password'),
     url(r'^token/?$', APITokenView.as_view(), name='account-api-token'),
