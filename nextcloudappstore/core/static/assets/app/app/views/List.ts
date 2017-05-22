5bd7f9cf8b7004a66daabe7fd68db75e8a537d01
Bernhard Posselt <BernhardPosselt@users.noreply.github.com>
2017-05-22 22:16:28 +0200

    Lazy load images so that the front page loads less at once (#486)
    * Lazy load images so that the front page loads less at once
    
    * fix style
    
    * fix style
    
    * do not remove centering for all elements after only one was loaded

--- nextcloudappstore/core/static/assets/app/app/views/List.ts
+++ nextcloudappstore/core/static/assets/app/app/views/List.ts
@@ -1,5 +1,7 @@
+import * as Blazy from 'blazy';
 import {queryOrThrow, ready} from '../../dom/Facades';
 import {findFormFields, FormField} from '../../forms/HtmlForm';
+import {Maybe} from '../../Utils';
 
 ready.then(() => {
     const form = queryOrThrow('#filter-form', HTMLFormElement);
@@ -8,5 +10,13 @@ ready.then(() => {
         elem.addEventListener('change', () => {
             form.submit();
         });
+    });
+    // tslint:disable-next-line no-unused-expression
+    new Blazy({
+        container: '#container',
+        success: (elem) => {
+            new Maybe(elem.closest('.app-list-screenshot'))
+                .ifPresent((el) => el.classList.remove('center'));
+        },
     });
 });
