78dd83ef523ddc73a2b86501e13c6c3e032894e9
Bernhard Posselt <BernhardPosselt@users.noreply.github.com>
2017-08-06 19:50:49 +0200

    fail build if coverage below 90% (#509)
    * fail build if coverage below 95
    
    * add a few test and relax to 90% coverage
    
    * add additional tests for user commands

---
+++ nextcloudappstore/user/management/commands/tests/test_createuser.py
@@ -0,0 +1,13 @@
+from django.contrib.auth import get_user_model
+from django.core.management import call_command
+from django.test import TestCase
+from django.utils.six import StringIO
+
+
+class CreateUserCommandTest(TestCase):
+    def test_create_user(self):
+        call_command('createtestuser', '--username=cmdtest',
+                     '--password=cmdtest', '--email=test@test.com',
+                     stdout=StringIO())
+        user = get_user_model().objects.get(username='cmdtest')
+        self.assertEqual('test@test.com', user.email)
---
+++ nextcloudappstore/user/management/commands/tests/test_setdefaultadminpassword.py
@@ -0,0 +1,18 @@
+from django.contrib.auth import get_user_model
+from django.core.management import call_command
+from django.test import TestCase
+from django.utils.six import StringIO
+
+
+class SetDefaultAdminPasswordCommandTest(TestCase):
+    fixtures = [
+        'admin.json',
+    ]
+
+    def test_set_password(self):
+        user = get_user_model().objects.get(username='admin')
+        user.set_password('different')
+        user.save()
+        call_command('setdefaultadminpassword', stdout=StringIO())
+        user.refresh_from_db()
+        self.assertTrue(user.check_password('admin'))
