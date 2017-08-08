aa6702ec68c21c7899bb6aebbb76d58424c39db2
Bernhard Posselt <dev@bernhard-posselt.com>
2017-08-08 11:46:06 +0200

    test for wrong email address on account delete

--- nextcloudappstore/core/tests/e2e/test_delete_account.py
+++ nextcloudappstore/core/tests/e2e/test_delete_account.py
@@ -6,9 +6,19 @@ class DeleteAccountTest(BaseStoreTest):
         self.login()
         self.go_to('user:account-deletion')
 
+        # test non matching address
+        self.by_id('id_email').send_keys('livetest@localhos')
+        self.by_id('id_email').submit()
+
+        self.wait_for('.text-danger',
+                      lambda el: self.assertTrue(el.is_displayed()))
+
+        # test valid address
+        self.by_id('id_email').clear()
         self.by_id('id_email').send_keys('livetest@localhost')
         self.by_id('id_email').submit()
 
+        # check if not able to login anymore
         self.go_to_login()
         self.by_id('id_login').send_keys('livetest')
         self.by_id('id_password').send_keys('livetest')
