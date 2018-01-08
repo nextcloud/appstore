91f963eca028df4ce1458631ffb2ff3f538eebb9
Bernhard Posselt <dev@bernhard-posselt.com>
2018-01-08 19:52:34 +0100

    undo postgres password changes

--- scripts/development/settings/postgres.py
+++ scripts/development/settings/postgres.py
@@ -1,6 +1,6 @@
 DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql'
 DATABASES['default']['NAME'] = 'nextcloudappstore'
-DATABASES['default']['USER'] = 'nextcloudappstore'
-DATABASES['default']['PASSWORD'] = 'nextcloudappstore'
+DATABASES['default']['USER'] = 'postgres'
+DATABASES['default']['PASSWORD'] = ''
 DATABASES['default']['PORT'] = '5432'
 DATABASES['default']['HOST'] = '127.0.0.1'
