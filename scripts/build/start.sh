5ec257faa17606ddaca76d135e8bf77777d15e74
Bernhard Posselt <dev@bernhard-posselt.com>
2018-01-13 16:27:45 +0100

    add start script

---
+++ scripts/build/start.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -e
+
+chown nextcloudappstore:nextcloudappstore -R /srv/logs
+chown nextcloudappstore:nextcloudappstore -R /srv/media
+
+# adjust database schema and load new data into it
+python manage.py migrate
+python manage.py loaddata nextcloudappstore/**/fixtures/*.json
+python manage.py importdbtranslations
+
+# copy data served by web server data into mounted volume
+python manage.py collectstatic --noinput
+
+exec "$@"
