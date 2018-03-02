57a75fbe2d7d03392bc021a806beb9785ed77860
rakekniven <mark.ziegler@rakekniven.de>
2018-03-02 18:41:56 +0100

    Update upload.html (#554)
    * Update upload.html
    
    Direct link to https.
    
    Just seen rewrite is working.
    
    * Update base.html
    
    Another change proposed at Transifex.
    See https://www.transifex.com/nextcloud/nextcloud/translate/#de/$/124614681?issue=yes
    
    * Update validators.py
    
    Fixed another tiny thing.
    Reported at Transifex. See https://www.transifex.com/nextcloud/nextcloud/translate/#de/$/96681342?issue=yes

--- nextcloudappstore/core/validators.py
+++ nextcloudappstore/core/validators.py
@@ -3,7 +3,7 @@ from django.utils.translation import ugettext_lazy as _  # type: ignore
 
 
 class HttpsUrlValidator(URLValidator):
-    message = _('Enter a valid HTTPS Url')
+    message = _('Enter a valid HTTPS URL')
 
     def __init__(self):
         super().__init__(schemes=['https'])
