45047dec038d28852192e954d94dd368eec1a50d
Bernhard Posselt <dev@bernhard-posselt.com>
2018-05-18 11:41:56 +0200

    fix race condition part 2

--- nextcloudappstore/core/tests/e2e/test_change_language.py
+++ nextcloudappstore/core/tests/e2e/test_change_language.py
@@ -1,6 +1,8 @@
+from selenium.webdriver.support.expected_conditions import staleness_of
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.support.wait import WebDriverWait
 
+from nextcloudappstore.core.tests.e2e import SELENIUM_WAIT_SEC
 from nextcloudappstore.core.tests.e2e.base import BaseStoreTest
 
 from django.test import tag
@@ -17,6 +19,6 @@ class ChangeLanguageTest(BaseStoreTest):
         lang.select_by_value('de')
         elem.submit()
 
-        self.go_to('user:account-change-language')
+        WebDriverWait(self.selenium, SELENIUM_WAIT_SEC).until(staleness_of(elem))
         account_link = self.findNavigationLink('user:account')
         self.assertEqual('Konto', account_link.text)
