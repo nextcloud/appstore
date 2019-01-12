c415e8fd7835e16ad2ea6babddbecd9871b4e82c
Bernhard Posselt <dev@bernhard-posselt.com>
2019-01-12 14:39:51 +0100

    do not use selenium for fetching feeds since firefox now prompts you to download them

--- nextcloudappstore/core/tests/e2e/test_feeds.py
+++ nextcloudappstore/core/tests/e2e/test_feeds.py
@@ -1,3 +1,5 @@
+import requests
+
 from nextcloudappstore.core.tests.e2e.base import BaseStoreTest
 
 from django.test import tag
@@ -18,13 +20,11 @@ class FeedTest(BaseStoreTest):
         self.go_to('home')
 
         rss_url = self.by_css('link[type="application/rss+xml"]')
-        self.selenium.get(rss_url.get_attribute('href'))
 
-        result = self.selenium.page_source
+        response = requests.get(rss_url.get_attribute('href'))
+        result = response.text
 
-        # unfortunately there is no selenium API to get the response code
-        # and firefox has custom rss feed handling so comparing the page
-        # source won't work
+        self.assertEqual(200, response.status_code)
         self.assertTrue(len(result) > 0)
         self.assertIn('News (10.1.0)', result)
 
@@ -32,12 +32,9 @@ class FeedTest(BaseStoreTest):
         self.go_to('home')
 
         rss_url = self.by_css('link[type="application/atom+xml"]')
-        self.selenium.get(rss_url.get_attribute('href'))
+        response = requests.get(rss_url.get_attribute('href'))
+        result = response.text
 
-        result = self.selenium.page_source
-
-        # unfortunately there is no selenium API to get the response code
-        # and firefox has custom rss feed handling so comparing the page
-        # source won't work
+        self.assertEqual(200, response.status_code)
         self.assertTrue(len(result) > 0)
         self.assertIn('News (10.1.0)', result)
