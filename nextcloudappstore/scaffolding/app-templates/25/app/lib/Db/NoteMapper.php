b13e46de0b5c0cabbb40aac8807a1a32b1108c09
Carl Schwan <carl@carlschwan.eu>
2022-11-29 15:42:54 +0100

    Add 25 template
    Signed-off-by: Carl Schwan <carl@carlschwan.eu>

---
+++ nextcloudappstore/scaffolding/app-templates/25/app/lib/Db/NoteMapper.php
@@ -0,0 +1,48 @@
+<?php
+declare(strict_types=1);
+// SPDX-FileCopyrightText: {{ app.author_name }} <{{ app.author_mail }}>
+// SPDX-License-Identifier: {{ app.license }}
+
+namespace OCA\{{ app.namespace }}\Db;
+
+use OCP\AppFramework\Db\DoesNotExistException;
+use OCP\AppFramework\Db\Entity;
+use OCP\AppFramework\Db\QBMapper;
+use OCP\DB\QueryBuilder\IQueryBuilder;
+use OCP\IDBConnection;
+
+/**
+ * @template-extends QBMapper<Note>
+ */
+class NoteMapper extends QBMapper {
+	public function __construct(IDBConnection $db) {
+		parent::__construct($db, '{{ app.id }}', Note::class);
+	}
+
+	/**
+	 * @throws \OCP\AppFramework\Db\MultipleObjectsReturnedException
+	 * @throws DoesNotExistException
+	 */
+	public function find(int $id, string $userId): Note {
+		/* @var $qb IQueryBuilder */
+		$qb = $this->db->getQueryBuilder();
+		$qb->select('*')
+			->from('{{ app.id }}')
+			->where($qb->expr()->eq('id', $qb->createNamedParameter($id, IQueryBuilder::PARAM_INT)))
+			->andWhere($qb->expr()->eq('user_id', $qb->createNamedParameter($userId)));
+		return $this->findEntity($qb);
+	}
+
+	/**
+	 * @param string $userId
+	 * @return array
+	 */
+	public function findAll(string $userId): array {
+		/* @var $qb IQueryBuilder */
+		$qb = $this->db->getQueryBuilder();
+		$qb->select('*')
+			->from('{{ app.id }}')
+			->where($qb->expr()->eq('user_id', $qb->createNamedParameter($userId)));
+		return $this->findEntities($qb);
+	}
+}
