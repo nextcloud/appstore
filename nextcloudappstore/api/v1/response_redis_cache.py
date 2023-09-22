18e479a65164b48e637139ce84c09fa92201f68b
Alexander Piskun <bigcat88@icloud.com>
2023-09-22 16:21:59 +0300

    adjusted cache TTL values
    Signed-off-by: Alexander Piskun <bigcat88@icloud.com>

--- nextcloudappstore/api/v1/response_redis_cache.py
+++ nextcloudappstore/api/v1/response_redis_cache.py
@@ -21,8 +21,7 @@ def condition_redis(etag_func):
         @wraps(func)
         def inner(request, *args, **kwargs):
             # The value from etag_func() could be quoted or unquoted.
-            res_etag = etag_func(request, *args, **kwargs) if etag_func else None
-            res_etag = quote_etag(res_etag) if res_etag is not None else None
+            res_etag = quote_etag(etag_func(request, *args, **kwargs))
 
             response = get_conditional_response(
                 request,
@@ -34,12 +33,14 @@ def condition_redis(etag_func):
                 if not cached_response:
                     response = func(request, *args, **kwargs)
                     if response.status_code == 200:
-                        cache.set(res_etag, response.rendered_content, 10 * 60)
+                        # apps.json size > 12 MB, to store 100 caches we need 1.2 GB
+                        # let assume we don't get more than 100 release updates in 3 minutes.
+                        cache.set(res_etag, response.rendered_content, 180)
                 else:
+                    cache.touch(res_etag, 90)
                     response = HttpResponse(content=cached_response, content_type="application/json")
             if request.method in ("GET", "HEAD"):
-                if res_etag:
-                    response.headers.setdefault("ETag", res_etag)
+                response.headers.setdefault("ETag", res_etag)
             return response
 
         return inner
