a0a981a78c46784866cd3be71faebd9c5ba6963c
Alexander Piskun <bigcat88@icloud.com>
2023-09-24 01:03:19 +0300

    to calm tslint
    Signed-off-by: Alexander Piskun <bigcat88@icloud.com>

--- nextcloudappstore/core/static/assets/app/dom/MarkdownSpec.ts
+++ nextcloudappstore/core/static/assets/app/dom/MarkdownSpec.ts
@@ -7,7 +7,8 @@ describe('Markdown rendering', () => {
         const expected = '<p>' +
             '<a href="http://google.com" rel="noopener noreferrer">link</a>' +
             '</p>\n' +
-            '<pre><code class="language-js"><span class="hljs-title function_">alert</span>(<span class="hljs-number">' +
+            '<pre><code class="language-js">' +
+            '<span class="hljs-title function_">alert</span>(<span class="hljs-number">' +
             '1</span>);\n</code></pre>';
         expect(renderMd(md).trim()).toEqual(expected.trim());
     });
