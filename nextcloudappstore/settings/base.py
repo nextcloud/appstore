0b77ead7dd02a97d17b6846c7052f5aa2c98071a
Alexander Piskun <bigcat88@icloud.com>
2023-09-25 09:10:13 +0300

    adjusted code to reflect Breaking Changes
    Signed-off-by: Alexander Piskun <bigcat88@icloud.com>

--- nextcloudappstore/settings/base.py
+++ nextcloudappstore/settings/base.py
@@ -55,6 +55,7 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "django.middleware.locale.LocaleMiddleware",
+    "allauth.account.middleware.AccountMiddleware",
 ]
 
 ROOT_URLCONF = "nextcloudappstore.urls"
