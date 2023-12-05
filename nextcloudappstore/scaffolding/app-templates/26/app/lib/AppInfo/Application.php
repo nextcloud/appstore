fe1bf41a7514d54308a409b5cc2a722c128f26c3
Daniel Kesselberg <mail@danielkesselberg.de>
2023-12-05 12:36:07 +0100

    feat: use IBootstrap
    Signed-off-by: Daniel Kesselberg <mail@danielkesselberg.de>

--- nextcloudappstore/scaffolding/app-templates/26/app/lib/AppInfo/Application.php
+++ nextcloudappstore/scaffolding/app-templates/26/app/lib/AppInfo/Application.php
@@ -6,11 +6,31 @@ declare(strict_types=1);
 namespace OCA\{{ app.namespace }}\AppInfo;
 
 use OCP\AppFramework\App;
+use OCP\AppFramework\Bootstrap\IBootContext;
+use OCP\AppFramework\Bootstrap\IBootstrap;
+use OCP\AppFramework\Bootstrap\IRegistrationContext;
 
-class Application extends App {
+class Application extends App implements IBootstrap {
 	public const APP_ID = '{{ app.id }}';
 
 	public function __construct() {
 		parent::__construct(self::APP_ID);
 	}
+
+
+    public function register(IRegistrationContext $context): void {
+        /*
+         * For further information about the app bootstrapping, please refer to our documentation:
+         * https://docs.nextcloud.com/server/latest/developer_manual/app_development/bootstrap.html
+         */
+        // Register your services, event listeners, etc.
+    }
+
+    public function boot(IBootContext $context): void {
+        /*
+         * For further information about the app bootstrapping, please refer to our documentation:
+         * https://docs.nextcloud.com/server/latest/developer_manual/app_development/bootstrap.html
+         */
+        // Prepare your app.
+    }
 }
