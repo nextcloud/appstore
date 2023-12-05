011e5b541cc46812adc72cf7927dd012f1a01363
Daniel Kesselberg <mail@danielkesselberg.de>
2023-12-05 12:33:16 +0100

    chore: add app-template for 26
    Signed-off-by: Daniel Kesselberg <mail@danielkesselberg.de>

---
+++ nextcloudappstore/scaffolding/app-templates/26/app/.eslintrc.js
@@ -0,0 +1,7 @@
+// SPDX-FileCopyrightText: {{ app.author_name }} <{{ app.author_mail }}>
+// SPDX-License-Identifier: {{ app.license }}
+module.exports = {
+	extends: [
+		'@nextcloud',
+	]
+}
---
+++ nextcloudappstore/scaffolding/app-templates/26/app/.php-cs-fixer.dist.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+// SPDX-FileCopyrightText: {{ app.author_name }} <{{ app.author_mail }}>
+// SPDX-License-Identifier: {{ app.license }}
+
+require_once './vendor/autoload.php';
+
+use Nextcloud\CodingStandard\Config;
+
+$config = new Config();
+$config
+	->getFinder()
+	->ignoreVCSIgnored(true)
+	->notPath('build')
+	->notPath('l10n')
+	->notPath('src')
+	->notPath('vendor')
+	->in(__DIR__);
+return $config;
