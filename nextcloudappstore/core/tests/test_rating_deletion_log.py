87ead794de76fce559088dc9e93b30fed84b1343
Alexander Piskun <bigcat88@icloud.com>
2023-12-30 15:32:01 +0300

    added small test
    Signed-off-by: Alexander Piskun <bigcat88@icloud.com>

---
+++ nextcloudappstore/core/tests/test_rating_deletion_log.py
@@ -0,0 +1,31 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+
+from nextcloudappstore.core.models import App, AppRating, AppRatingDeleteLog
+
+
+class AppRatingDeletionLogTest(TestCase):
+    def test_delete_app(self):
+        user = get_user_model().objects.create(username="doe")
+        app_rating = AppRating.objects.create(app=App.objects.create(owner=user, id="news"), user=user)
+        self.assertEqual(0, AppRatingDeleteLog.objects.count())
+
+        app_rating.delete()
+        self.assertEqual(1, AppRatingDeleteLog.objects.count())
+
+    def test_delete_owner(self):
+        user = get_user_model().objects.create(username="doe")
+        AppRating.objects.create(app=App.objects.create(owner=user, id="news"), user=user)
+        self.assertEqual(0, AppRatingDeleteLog.objects.count())
+
+        user.delete()
+        self.assertEqual(1, AppRatingDeleteLog.objects.count())
+
+    def test_delete_app_rating(self):
+        user = get_user_model().objects.create(username="doe")
+        app = App.objects.create(owner=user, id="news")
+        app_rating = AppRating.objects.create(app=app, user=user)
+        self.assertEqual(0, AppRatingDeleteLog.objects.count())
+        app_rating.delete()
+
+        self.assertEqual(1, AppRatingDeleteLog.objects.count())
