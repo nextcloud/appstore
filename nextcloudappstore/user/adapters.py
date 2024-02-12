f7af808b8a7430148f96cc0738ac4722786e7d56
Alexander Piskun <bigcat88@icloud.com>
2024-02-12 21:52:00 +0300

    added email validation for signup
    Signed-off-by: Alexander Piskun <bigcat88@icloud.com>

---
+++ nextcloudappstore/user/adapters.py
@@ -0,0 +1,13 @@
+from allauth.account.adapter import DefaultAccountAdapter
+from django import forms
+from django.utils.translation import gettext_lazy as _
+from email_validator import EmailNotValidError, validate_email
+
+
+class CustomAccountAdapter(DefaultAccountAdapter):
+    def clean_email(self, email):
+        try:
+            validate_email(email, check_deliverability=True, timeout=3)
+        except EmailNotValidError:
+            raise forms.ValidationError(_("Provided email address is not valid."))
+        return email
