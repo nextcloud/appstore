7860a50d6c64a6444b98794a6dacd0a15a967ef7
provokateurin <kate@provokateurin.de>
2024-04-29 15:34:27 +0200

    refactor(scaffolding): Migrate app template to Github template repo
    Signed-off-by: provokateurin <kate@provokateurin.de>

--- nextcloudappstore/scaffolding/archive.py
+++ nextcloudappstore/scaffolding/archive.py
@@ -2,56 +2,64 @@ import re
 import tarfile
 from io import BytesIO
 from os import walk
-from os.path import isdir, join, relpath
-
-from django.conf import settings
-from django.template import Context, Template
+from os.path import isdir, join, relpath, basename
 
 from nextcloudappstore.core.facades import resolve_file_relative_path
 
 
 def build_files(args: dict[str, str]) -> dict[str, str]:
-    platform = int(args["platform"])  # prevent path traversal
-    vars = {
-        "id": args["name"].lower(),
-        "summary": args["summary"],
-        "description": args["description"],
-        "name": " ".join(re.findall(r"[A-Z][^A-Z]*", args["name"])),
-        "namespace": args["name"],
-        "author_name": args["author_name"],
-        "author_mail": args["author_email"],
-        "author_homepage": args["author_homepage"],
-        "issue_tracker": args["issue_tracker"],
-        "categories": args["categories"],
-        "nextcloud_version": platform,
-        "license": "AGPL-3.0-or-later",
+    id = args["name"].lower()
+    name = " ".join(re.findall(r"[A-Z][^A-Z]*", args["name"]))
+    summary = args["summary"]
+    description = args["description"]
+    author_name = args["author_name"]
+    author_mail = args["author_email"]
+    author_homepage = args["author_homepage"]
+    namespace = args["name"]
+    categories = args["categories"]
+    issue_tracker = args["issue_tracker"]
+    patterns = {
+        "app_template": id,
+        "App Template": name,
+        "AppTemplate": namespace,
+        "An example summary": summary,
+        "An example description": description,
+        "<category>customization</category>": "\n\t".join(
+            map(lambda category: f"<category>{category}</category>", categories)),
+        "<bugs>https://example.com/bugs</bugs>": f"<bugs>{issue_tracker}</bugs>",
+        '"name": "example"': f'"name": "{author_name}"',
+        '"email": "example@example.com"': f'"email": "{author_mail}"',
     }
-    vars.update(settings.APP_SCAFFOLDING_PROFILES.get(platform, {}))
-    relative_base = "app-templates/%i/app/" % platform
-    base = resolve_file_relative_path(__file__, relative_base)
 
-    context = Context({"app": vars})
+    if author_homepage is not None:
+        patterns[
+            '<author mail="example@example.com" homepage="https://example.com">Example</author>'] = f'<author mail="{author_mail}" homepage="{author_homepage}">{author_name}</author>'
+        patterns['"homepage": "https://example.com"'] = f'"homepage": "{author_homepage}"'
+    else:
+        patterns[
+            '<author mail="example@example.com" homepage="https://example.com">Example</author>'] = f'<author mail="{author_mail}">{author_name}</author>'
+        patterns['"homepage": "https://example.com"'] = f'"homepage": ""'
+
+    base = resolve_file_relative_path(__file__, "app_template")
     result = {}
     if not isdir(base):
         return result
 
     for root, dirs, files in walk(base):
         for file in files:
+            # Exclude lockfiles which are no longer valid after editing the content
+            if basename(file) == "composer.lock" or basename(file) == "package-lock.json":
+                continue
+
             file_path = join(root, file)
-            rel_file_path = "{}/{}".format(vars["id"], relpath(file_path, base))
+            rel_file_path = "{}/{}".format(id, relpath(file_path, base))
             with open(file_path, encoding="utf-8") as f:
-                t = Template(f.read())
-                result[rel_file_path] = apply_github_actions_fixer(rel_file_path, t.render(context))
+                data = f.read()
+                for pattern, replacement in patterns.items():
+                    data = data.replace(pattern, replacement)
+
+                result[rel_file_path] = data
     return result
-
-
-def apply_github_actions_fixer(file_path: str, file_content: str) -> str:
-    if file_path.find(".github/") == -1 or not file_path.endswith(".yml"):
-        return file_content
-    fixed_content = file_content.lstrip("\n")
-    if fixed_content.endswith("\n\n"):
-        fixed_content = fixed_content[: len(fixed_content) - 1]
-    return fixed_content
 
 
 def build_archive(parameters: dict[str, str]) -> BytesIO:
--- nextcloudappstore/scaffolding/tests/test_archive.py
+++ nextcloudappstore/scaffolding/tests/test_archive.py
@@ -4,7 +4,6 @@ from django.test import TestCase
 
 from nextcloudappstore.core.facades import read_relative_file
 from nextcloudappstore.scaffolding.archive import (
-    apply_github_actions_fixer,
     build_archive,
     build_files,
 )
@@ -19,48 +18,23 @@ class ArchiveTest(TestCase):
             "author_name": "author name",
             "author_email": "author email",
             "author_homepage": "author homepage",
-            "platform": "28",
             "issue_tracker": "https://test.com",
             "categories": ["tools"],
         }
 
     def test_build_files(self):
-        with self.settings(APP_SCAFFOLDING_PROFILES={28: {"owncloud_version": "9.2"}}):
-            expected = read_relative_file(__file__, "data/info.xml").strip()
-            result = build_files(self.args)
-            info = result["theapp/appinfo/info.xml"].strip()
-            self.assertEqual(expected, info)
-
-    def test_no_int_version(self):
-        with self.assertRaises(ValueError):
-            build_files({"platform": "test"})
-
-    def test_no_directory(self):
-        self.args["platform"] = "8"
+        expected = read_relative_file(__file__, "data/info.xml")
         result = build_files(self.args)
-        self.assertDictEqual({}, result)
+        info = result["theapp/appinfo/info.xml"]
+        self.assertEqual(expected, info)
+        self.assertNotIn("package-lock.json", result)
+        self.assertNotIn("composer.lock", result)
 
     def test_build_archive(self):
-        with self.settings(APP_SCAFFOLDING_PROFILES={28: {"owncloud_version": "9.2"}}):
-            result = build_archive(self.args)
-            expected = read_relative_file(__file__, "data/info.xml").strip()
-            with tarfile.open(fileobj=result, mode="r:gz") as f:
-                member = f.getmember("theapp/appinfo/info.xml")
-                with f.extractfile(member) as info:
-                    result = info.read().strip().decode("utf-8")
-                    self.assertEqual(expected, result)
-
-    def test_build_files_github_actions_format(self):
-        for file_path, file_content in build_files(self.args).items():
-            if file_path.find(".github/") != -1 and file_path.endswith(".yml"):
-                assert not file_content.startswith("\n")
-                assert not file_content.endswith("\n\n")
-                assert file_content.endswith("\n")
-
-    def test_apply_github_actions_fixer(self):
-        for rel_path in (".github/CODE_OF_CONDUCT.md", ".github/workflows/reuse."):
-            assert apply_github_actions_fixer(rel_path, "\nbody\n\n") == "\nbody\n\n"
-        for rel_path in (".github/test.yml", ".github/workflows/test.yml"):
-            assert apply_github_actions_fixer(rel_path, "\nbody\n\n") == "body\n"
-        for file_content in ("body\n\n", "\nbody\n", "body\n"):
-            assert apply_github_actions_fixer(".github/test.yml", file_content) == "body\n"
+        result = build_archive(self.args)
+        expected = read_relative_file(__file__, "data/info.xml")
+        with tarfile.open(fileobj=result, mode="r:gz") as f:
+            member = f.getmember("theapp/appinfo/info.xml")
+            with f.extractfile(member) as info:
+                result = info.read().decode("utf-8")
+                self.assertEqual(expected, result)
